0af6c5f0060bfbf9a7734f543100b0d5bbd8d7ef
thinkcreative-matt <56924270+thinkcreative-matt@users.noreply.github.com>
2020-10-09 15:00:16 +0100

    pluralised makers in message

--- resources/js/roulette/removeNameFromList.js
+++ resources/js/roulette/removeNameFromList.js
@@ -7,7 +7,7 @@ const removeNameFromList = (e) => {
     const names = document.querySelectorAll('#names > li');
 
     if(names.length <= 2){  
-        return message("You can't have less than 2 brew maker.");
+        return message("You can't have less than 2 brew makers");
     }
 
     let el = e.target;
