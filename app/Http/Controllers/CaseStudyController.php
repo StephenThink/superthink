5bd55c057a8795288a1601084385eb5c06661458
thinkcreative-matt <56924270+thinkcreative-matt@users.noreply.github.com>
2021-05-25 19:19:32 +0100

    tidy up the case studies returned title

--- app/Http/Controllers/CaseStudyController.php
+++ app/Http/Controllers/CaseStudyController.php
@@ -14,14 +14,17 @@ class CaseStudyController extends Controller
         $byService = $caseStudies->filter(function ($store) use ( $service ) {
             if( is_array($store->services_filter) && in_array( $service, $store->services_filter ) )
                 return $store->services_filter;
-        })->toArray();
+        });
+
+        if( $byService->isEmpty() )
+            throw new \Statamic\Exceptions\NotFoundHttpException;
 
         return (new \Statamic\View\View)
             ->template('case_studies.all')
             ->layout('layout')
             ->with([
-                'title' => \Illuminate\Support\Str::title($service),
-                'data' => $byService
+                'title' => str_replace('-', ' ', \Illuminate\Support\Str::title($service) ),
+                'data' => $byService->toArray()
             ]);
 
     }
