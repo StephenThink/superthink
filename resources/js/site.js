8a1a64f10c78ad3e71d174c91bfebd02d03ce7bc
Stephen Jackson <jjackson19862017@gmail.com>
2021-10-13 10:15:46 +0100

    Dark Dropdown for the Case Studies next case.

--- resources/js/site.js
+++ resources/js/site.js
@@ -90,9 +90,9 @@ if (localStorage['thinkcreative.theme'] === 'dark' || (!('thinkcreative.theme' i
 }
 
 //  Always be at the top when entering the page.
-// barba.hooks.enter(() => {
-//     window.scrollTo(0, 0);
-// });
+barba.hooks.enter(() => {
+    window.scrollTo(0, 0);
+});
 
 //  Always close the navigation when leaving a page. 
 // barba.hooks.beforeLeave(() => {
@@ -102,22 +102,22 @@ if (localStorage['thinkcreative.theme'] === 'dark' || (!('thinkcreative.theme' i
 //     handle.classList.remove('open');
 // });
 
-// barba.init({
-//     transitions: [{
-//         name: 'opacity-transition',
-//         leave(data) {
+barba.init({
+    transitions: [{
+        name: 'opacity-transition',
+        leave(data) {
 
-//             return gsap.to(data.current.container, {
-//                 opacity: 0
-//             }, 5);
+            return gsap.to(data.current.container, {
+                opacity: 0
+            }, 5);
 
-//         },
-//         enter(data) {
-//             inView('.fancy:not(.seen)').on('enter', animateIn);
-//             console.log('ENTERING');
-//             return gsap.from(data.next.container, {
-//                 opacity: 0
-//             });
-//         }
-//       }]
-// });
+        },
+        enter(data) {
+            inView('.fancy:not(.seen)').on('enter', animateIn);
+            console.log('ENTERING');
+            return gsap.from(data.next.container, {
+                opacity: 0
+            });
+        }
+      }]
+});
