d1e978bf49a66d8464bc678ba6f79c0fecf6543f
StephenThink <86749311+StephenThink@users.noreply.github.com>
2022-02-11 10:16:24 +0000

    Container Restored

--- tailwind.config.js
+++ tailwind.config.js
@@ -47,7 +47,11 @@ module.exports = {
       padding: '2rem',
       center: true,
       screens: {
-       
+        "none": "100%",
+        sm: '540px',
+        md: '720px',
+        lg: '960px',
+        xl: '1140px',
       }
     },
     fontFamily: {
