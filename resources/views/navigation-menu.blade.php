e211bf37ec50c25610988c93604fec9471aa050b
StephenThink <86749311+StephenThink@users.noreply.github.com>
2022-02-16 12:16:04 +0000

    Tidying up Nav Menu

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,24 +15,14 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('pages') }}" :active="request()->routeIs('pages')">
-                        {{ __('Pages') }}
-                    </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('navigation-menus') }}" :active="request()->routeIs('navigation-menus')">
-                        {{ __('Navigation Menus') }}
-                    </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('users') }}" :active="request()->routeIs('users')">
-                        {{ __('Users') }}
-                    </x-jet-nav-link>
-                    <x-jet-nav-link href="{{ route('user-permissions') }}" :active="request()->routeIs('user-permissions')">
-                        {{ __('User Permissions') }}
-                    </x-jet-nav-link>
+
+
                 </div>
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <!-- Teams Dropdown -->
-                @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
+                {{-- @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
                     <div class="ml-3 relative">
                         <x-jet-dropdown align="right" width="60">
                             <x-slot name="trigger">
@@ -79,7 +69,56 @@
                             </x-slot>
                         </x-jet-dropdown>
                     </div>
-                @endif
+                @endif --}}
+
+<!-- Admin Dropdown -->
+<div class="ml-3 relative">
+    <x-jet-dropdown align="right" width="60">
+        <x-slot name="trigger">
+            <span class="inline-flex rounded-md">
+                <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition">
+                    {{ __('Admin') }}
+
+                    <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+                    </svg>
+                </button>
+            </span>
+        </x-slot>
+
+        <x-slot name="content">
+            <div class="w-60">
+                <!-- Frontend Management -->
+                <div class="block px-4 py-2 text-xs text-gray-400">
+                    {{ __('Manage Frontend') }}
+                </div>
+
+                <!-- Pages / Nav Options -->
+
+                <x-jet-dropdown-link href="{{ route('pages') }}" :active="request()->routeIs('pages')">
+                    {{ __('Pages') }}
+                </x-jet-dropdown-link>
+                <x-jet-dropdown-link href="{{ route('navigation-menus') }}" :active="request()->routeIs('navigation-menus')">
+                    {{ __('Navigation Menus') }}
+                </x-jet-dropdown-link>
+
+                <!-- Users Management -->
+                <div class="block px-4 py-2 text-xs text-gray-400">
+                    {{ __('Manage Users') }}
+                </div>
+
+                <!-- Pages / Nav Options -->
+
+                <x-jet-dropdown-link href="{{ route('users') }}" :active="request()->routeIs('users')">
+                    {{ __('Users') }}
+                </x-jet-dropdown-link>
+                <x-jet-dropdown-link href="{{ route('user-permissions') }}" :active="request()->routeIs('user-permissions')">
+                    {{ __('User Permissions') }}
+                </x-jet-dropdown-link>
+            </div>
+        </x-slot>
+    </x-jet-dropdown>
+</div>
 
                 <!-- Settings Dropdown -->
                 <div class="ml-3 relative">
