08bb7546ee45afb7253c4fc1067f65a14a3d9e74
StephenThink <86749311+StephenThink@users.noreply.github.com>
2022-03-03 12:17:15 +0000

    Grabbing all the other Styles

--- resources/views/clients/profile.blade.php
+++ resources/views/clients/profile.blade.php
@@ -1,16 +1,16 @@
 <x-app-layout>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+    <div class="content-outer">
+        <div class="content-block">
+            <div class="content-inner">
                 @livewire('client-profile-contacts', ['id' => $id])
             </div>
         </div>
     </div>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+    <div class="content-outer">
+        <div class="content-block">
+            <div class="content-inner">
                 @livewire('client-profile', ['id' => $id])
             </div>
         </div>
